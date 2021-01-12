191c62d6b6f64d37e060e2ba8199d9ef11fd18dd
ted <tedmarov@gmail.com>
2021-01-12 13:30:29 -0600

    JSON on top

--- request_handler.py
+++ request_handler.py
@@ -1,9 +1,9 @@
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal
 from locations import get_all_locations, get_single_location
 from employees import get_all_employees, get_single_employee
 from customers import get_all_customers, get_single_customer
-import json
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
