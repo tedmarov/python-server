a1077ffbadca04d634a45fa2d4d3e34d6db4cb57
ted <tedmarov@gmail.com>
2021-01-12 10:11:33 -0600

    Started on chapter 4

--- request_handler.py
+++ request_handler.py
@@ -1,9 +1,9 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal
+from animals import get_all_animals, get_single_animal, create_animal
 from locations import get_all_locations, get_single_location
 from employees import get_all_employees, get_single_employee
 from customers import get_all_customers, get_single_customer
-
+import json
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -88,19 +88,27 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
     def do_POST(self):
-        # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
 
-    # Here's a method on the class that overrides the parent's method.
-    # It handles any PUT request.
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
 
-    def do_PUT(self):
-        self.do_POST()
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_animal = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write(f"{new_animal}".encode())
 
 
 # This function is not inside the class. It is the starting
