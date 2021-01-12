7336391c14ba68918055c441fa5553dcf7dd053e
ted <tedmarov@gmail.com>
2021-01-12 14:24:44 -0600

    Added deletes

--- customers/request.py
+++ customers/request.py
@@ -55,3 +55,18 @@ def create_customer(customer):
 
     # Return the dictionary with `id` property added
     return customer
+
+def delete_customer(id):
+    # Initial -1 value for customer index, in case one isn't found
+    customer_index = -1
+
+    # Iterate the customerS list, but use enumerate() so that you
+    # can access the index value of each item
+    for index, customer in enumerate(CUSTOMERS):
+        if customer["id"] == id:
+            # Found the customer. Store the current index.
+            customer_index = index
+
+    # If the customer was found, use pop(int) to remove it from list
+    if customer_index >= 0:
+        CUSTOMERS.pop(customer_index)
--- request_handler.py
+++ request_handler.py
@@ -1,9 +1,9 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal, create_animal
-from locations import get_all_locations, get_single_location, create_location
-from employees import get_all_employees, get_single_employee, create_employee
-from customers import get_all_customers, get_single_customer, create_customer
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal
+from locations import get_all_locations, get_single_location, create_location, delete_location
+from employees import get_all_employees, get_single_employee, create_employee, delete_employee
+from customers import get_all_customers, get_single_customer, create_customer, delete_customer
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -139,6 +139,34 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         # Encode the new customer and send in response
         self.wfile.write(f"{new_customer}".encode())
+
+def do_DELETE(self):
+    # Set a 204 response code
+    self._set_headers(204)
+
+    # Parse the URL
+    (resource, id) = self.parse_url(self.path)
+
+    # Delete a single animal from the list
+    if resource == "animals":
+        delete_animal(id)
+
+    # Delete a single location from the list
+    if resource == "locations":
+        delete_location(id)
+
+    # Delete a single employee from the list
+    if resource == "employees":
+        delete_employee(id)
+
+    # Delete a single customer from the list
+    if resource == "customers":
+        delete_customer(id)
+
+    # Encode the new item and send in response
+    self.wfile.write("".encode())
+
+
 
 # This function is not inside the class. It is the starting
 # point of this application.
