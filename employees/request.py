a268b90f28982c09c12a4e95e10d27303fb56d16
ted <tedmarov@gmail.com>
2021-01-14 15:16:46 -0600

    Finished

--- employees/request.py
+++ employees/request.py
@@ -144,21 +144,30 @@ def get_employees_by_location(location_id):
 
     return json.dumps(employees)
 
-def create_employee(employee):
-    # Get the id value of the last employee in the list
-    max_id = EMPLOYEES[-1]["id"]
+def save_employee(new_employee):
+    with sqlite3.connect("./kennel.db") as conn:
+        db_cursor = conn.cursor()
 
-    # Add 1 to whatever that number is
-    new_id = max_id + 1
+        db_cursor.execute("""
+        INSERT INTO Employee
+            ( name, address, location_id)
+        VALUES
+            ( ?, ?, ?);
+        """, (new_employee['name'], new_employee['address'],
+                new_employee['location_id'], ))
 
-    # Add an `id` property to the employee dictionary
-    employee["id"] = new_id
+        # The `lastrowid` property on the cursor will return
+        # the primary key of the last thing that got added to
+        # the database.
+        id = db_cursor.lastrowid
 
-    # Add the employee dictionary to the list
-    EMPLOYEES.append(employee)
+        # Add the `id` property to the animal dictionary that
+        # was sent by the client so that the client sees the
+        # primary key in the response.
+        new_employee['id'] = id
 
-    # Return the dictionary with `id` property added
-    return employee
+
+    return json.dumps(new_employee)
     
 def delete_employee(id):
     # Initial -1 value for employee index, in case one isn't found
