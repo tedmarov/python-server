69173be811dcd78a1b11fdd17326fafd868444d2
ted <tedmarov@gmail.com>
2021-01-14 14:17:37 -0600

    Fixed from previous, implementing CREATE

--- animals/request.py
+++ animals/request.py
@@ -94,7 +94,7 @@ def get_all_animals():
             animal.location = location.__dict__
 
             # Add the dictionary representation of the location to the animal
-            animal.location = customer.__dict__
+            animal.customer = customer.__dict__
 
             # Add the dictionary representation of the animal to the list
             animals.append(animal.__dict__)
@@ -192,21 +192,31 @@ def get_animals_by_status(status):
 
     return json.dumps(animals)
 
-def create_animal(animal):
-    # Get the id value of the last animal in the list
-    max_id = ANIMALS[-1]["id"]
+def create_animal(new_animal):
+    with sqlite3.connect("./kennel.db") as conn:
+        db_cursor = conn.cursor()
 
-    # Add 1 to whatever that number is
-    new_id = max_id + 1
+        db_cursor.execute("""
+        INSERT INTO Animal
+            ( name, breed, status, location_id, customer_id )
+        VALUES
+            ( ?, ?, ?, ?, ?);
+        """, (new_animal['name'], new_animal['species'],
+                new_animal['status'], new_animal['location_id'],
+                new_animal['customer_id'], ))
 
-    # Add an `id` property to the animal dictionary
-    animal["id"] = new_id
+        # The `lastrowid` property on the cursor will return
+        # the primary key of the last thing that got added to
+        # the database.
+        id = db_cursor.lastrowid
 
-    # Add the animal dictionary to the list
-    ANIMALS.append(animal)
+        # Add the `id` property to the animal dictionary that
+        # was sent by the client so that the client sees the
+        # primary key in the response.
+        new_animal['id'] = id
 
-    # Return the dictionary with `id` property added
-    return animal
+
+    return json.dumps(new_animal)
 
 def delete_animal(id):
     with sqlite3.connect("./kennel.db") as conn:
